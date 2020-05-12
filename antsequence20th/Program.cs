39d6757f8f2c8772346596c1de7f841358046c71
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-12 20:03:35 +0900

    0512 8번 문제 개미수열

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,34 @@ namespace antsequence20th
     {
         static void Main(String[] args)
         {
+            Console.Write("몇번째 수열?: ");
+            int num = int.Parse(Console.ReadLine()), cnt, i, j;
+            string current = "1", next;
+            char temp;
+
+            for (i = 0; i < num; i++) //수열 갯수?
+            {
+                Console.WriteLine(i + 1 + "번째 수열: " + current);
+                temp = current[0];
+                cnt = 0;
+                next = "";
+
+                for (j = 0; j < current.Length; j++)
+                {
+                    if (temp == current[j])//갯수 증가
+                        cnt++;
+                    else
+                    {
+                        next += "" + temp + cnt;
+                        temp = current[j];
+                        cnt = 1;
+                    }
+
+                    if (j == current.Length - 1)
+                        next += "" + temp + cnt;
+                }
+                current = next;
+            }
         }
     }
 }
